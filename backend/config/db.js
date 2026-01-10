6cbaa1f2820ce4fc102be829422f5f1ee97bc46f
Ashish Sharma <ashu11vats@gmail.com>
2026-01-10 18:20:19 +0530

    update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,4 +10,12 @@ const pool = mysql.createPool({
   connectionLimit: 10,
 });
 
+console.log("🧠 MySQL ENV Loaded →", {
+  MYSQLHOST: process.env.MYSQLHOST,
+  MYSQLUSER: process.env.MYSQLUSER,
+  MYSQLDATABASE: process.env.MYSQLDATABASE,
+  MYSQLPORT: process.env.MYSQLPORT,
+});
+
+
 export default pool.promise();
